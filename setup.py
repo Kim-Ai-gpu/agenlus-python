03d95c726488e2687e776741c04ad995ecfe8b95
Kim-Ai-gpu <dagdagadg1234@gmail.com>
2026-06-02 15:42:33 +0900

    docs: setup.py metadata update for PyPI

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Agenlus Team",
+    url="https://github.com/Kim-Ai-gpu/agenlus-python",
+    project_urls={
+        "Bug Tracker": "https://github.com/Kim-Ai-gpu/agenlus-python/issues",
+        "Source Code": "https://github.com/Kim-Ai-gpu/agenlus-python",
+    },
     packages=find_packages(),
     install_requires=[
         "requests",
@@ -19,4 +24,15 @@ setup(
         "huggingface_hub"
     ],
     python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
